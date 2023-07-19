e1033bc8c41c6df1c264dec9df3e77894b29202c
João Victor Novais <joao.dipaay@gmail.com>
2023-07-19 11:07:15 -0300

    Successful unit test

--- src/test/java/br/com/joaovictor/todolist/TodolistApplicationTests.java
+++ src/test/java/br/com/joaovictor/todolist/TodolistApplicationTests.java
@@ -1,16 +1,36 @@
 package br.com.joaovictor.todolist;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import br.com.joaovictor.todolist.entity.Todo;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class TodolistApplicationTests {
+	
+	@Autowired
+	private WebTestClient webTestClient;
 
 	@Test
 	void testCreateTodoSuccess() {
 		var todo = new Todo("todo 1", "todo 1 desc", false, 2);
+		
+		webTestClient
+			.post()
+			.uri("/todos")
+			.bodyValue(todo)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+			.jsonPath("$").isArray()
+			.jsonPath("$.length()").isEqualTo(1)
+			.jsonPath("$[0].name").isEqualTo(todo.getName())
+			.jsonPath("$[0].description").isEqualTo(todo.getDescription())
+			.jsonPath("$[0].completed").isEqualTo(todo.getCompleted())
+			.jsonPath("$[0].priority").isEqualTo(todo.getPriority());
 	}
 	
 	@Test
